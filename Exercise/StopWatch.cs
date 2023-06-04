e217d8f567599fb58fe1486c809c1e34137e30ed
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-06-04 17:31:37 +0530

    Add project files.

---
+++ Exercise/StopWatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_OOP.Exercise
+{
+    internal class StopWatch
+    {
+        private static StopWatch stp = null;
+        
+        
+        private DateTime start;
+        private DateTime stop;
+        private StopWatch() { }
+
+        public static StopWatch getStpInstance()
+        {
+            if( stp == null )
+            {
+                stp = new StopWatch();
+            }
+            return stp;
+        }
+
+        public void StartTheWatch()
+        {
+           
+            
+        }
+
+        public void StopTheWatch()
+        {
+            
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Console_OOP.Exercise;
+
+
+namespace Console_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jay Ganesh");
+
+            //StopWatch s = StopWatch.getStpInstance();
+            //Console.WriteLine(s.ToString()); 
+            
+            //StopWatch s1 = StopWatch.getStpInstance();
+
+        }
+    }
+}
