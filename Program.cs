3f5d1a1896a30e43b8793eeca56168b03e83f4d2
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-06-09 23:48:55 +0530

    s

--- Program.cs
+++ Program.cs
@@ -14,40 +14,7 @@ namespace Console_OOP
         static void Main(string[] args)
         {
             Console.WriteLine("Jay Ganesh");
-            //Mobile m = new Mobile();
-            //m.Price= 100;
-            //m.Description = "kjs99 23 9- 2-2-2-2-2";
-            //m.Name = "Note 1";
-            //m.show();           
-
-            //Sample1 s = new Sample1();
-            //s.Samples.Add(s);
-            int day = 4;
-            switch (day)
-            {
-                case 1:
-                    Console.WriteLine("Monday");
-                    break;
-                case 2:
-                    Console.WriteLine("Tuesday");
-                    break;
-                case 3:
-                    Console.WriteLine("Wednesday");
-                    break;
-                case 4:
-                    Console.WriteLine("Thursday");
-                    break;
-                case 5:
-                    Console.WriteLine("Friday");
-                    break;
-                case 6:
-                    Console.WriteLine("Saturday");
-                    break;
-                case 7:
-                    Console.WriteLine("Sunday");
-                    break;
-            }
-            // Outputs "Thursday" (day 4)
+            
 
 
         }
